47e8eb5cb83e3ea4e9007780edbb02c85452142d
Michele Ferrari <m.ferrari092@studenti.unibs.it>
2023-07-24 17:26:04 +0200

    CMakeLists bugfix

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <vector>
+#include <iostream>
+#include <ilqgames_planning/prova_header.h>
+
+#include <ilqgames/solver/ilq_solver.h>
+
+int main(int argc, char **argv)
+{
+  std::cout << "Successfully compiled" << std::endl;
+  return 0;
+}
